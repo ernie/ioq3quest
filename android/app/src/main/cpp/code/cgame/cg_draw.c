f9a91cd2da1c5f006dc829d8166f1d94f2740687
Ernie Miller <ernie@ernie.io>
2026-03-14 01:35:01 -0400

    Small TVD playback fixes
    - Scoreboard clicks not working if seeking back from intermission
    - Occasional cgane loading screen hang on multiple demo plays

--- android/app/src/main/cpp/code/cgame/cg_draw.c
+++ android/app/src/main/cpp/code/cgame/cg_draw.c
@@ -4108,7 +4108,8 @@ void CG_ResetSeekState( void ) {
 	// Damage vignette — CG_DamageBlendBlob: (cg.time - damageTime) vs DAMAGE_TIME
 	cg.damageTime = 0;
 
-	// Scoreboard
+	// Scoreboard / intermission
+	cg.intermissionStarted = qfalse;
 	cg.showScores = qfalse;
 	cg.scoreFadeTime = 0;
 	cg.scoreBoardShowing = qfalse;
