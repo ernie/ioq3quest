7f2d8bbf2129a6f9b594e0e898f325318c2e0114
Ernie Miller <ernie@ernie.io>
2026-03-22 15:59:17 -0400

    Add g_physics selection
    0 - Vanilla Quake 3
    1 - CPMA
    2 - Quake Live
    3 - Quake Live Turbo

--- android/app/src/main/cpp/code/cgame/cg_local.h
+++ android/app/src/main/cpp/code/cgame/cg_local.h
@@ -1190,6 +1190,7 @@ typedef struct {
 
 	qboolean		pmove_fixed;
 	int				pmove_msec;
+	int				pmove_physics;
 
 	qboolean		synchronousClients;
 
--- android/app/src/main/cpp/code/q3_ui/ui_preferences.c
+++ android/app/src/main/cpp/code/q3_ui/ui_preferences.c
@@ -62,9 +62,9 @@ GAME OPTIONS MENU
 #define ID_GORE 			    139
 #define ID_DAMAGEEFFECT		    140
 #define ID_SHOWINHAND		    141
-#define ID_SELECTORWITHHUD		142
-#define ID_SHOWCONSOLE			143
-#define ID_BACK					144
+#define ID_SHOWCONSOLE			142
+#define ID_BACK					143
+#define ID_PHYSICS				144
 
 #define	NUM_CROSSHAIRS			10
 #define	NUM_GORE    			4
@@ -92,8 +92,8 @@ typedef struct {
 	menulist_s 			gore;
 	menulist_s			damageeffect;
 	menuradiobutton_s	showinhand;
-	menuradiobutton_s	selectorwithhud;
 	menuradiobutton_s	showconsole;
+	menulist_s			physics;
 	menubitmap_s		back;
 
 	qhandle_t			crosshairShader[NUM_CROSSHAIRS];
@@ -136,6 +136,15 @@ static const char *s_damageeffect[] =
 	NULL
 };
 
+static const char *physics_names[] =
+{
+	"Vanilla Quake 3",
+	"CPMA",
+	"Quake Live",
+	"Quake Live Turbo",
+	NULL
+};
+
 static int gamecodetoui[] = {4,2,3,0,5,1,6};
 static int uitogamecode[] = {4,6,2,3,1,5,7};
 
@@ -163,8 +172,8 @@ static void Preferences_SetMenuItems( void ) {
 	s_preferences.gore.curvalue				= trap_Cvar_VariableValue( "vr_goreLevel" );
 	s_preferences.damageeffect.curvalue		= trap_Cvar_VariableValue( "cg_damageEffect" );
 	s_preferences.showinhand.curvalue		= trap_Cvar_VariableValue( "vr_showItemInHand" ) != 0;
-	s_preferences.selectorwithhud.curvalue	= trap_Cvar_VariableValue( "vr_weaponSelectorWithHud" ) != 0;
 	s_preferences.showconsole.curvalue		= trap_Cvar_VariableValue( "vr_showConsoleMessages" ) != 0;
+	s_preferences.physics.curvalue			= Com_Clamp( 0, 3, trap_Cvar_VariableValue( "ui_physics" ) );
 }
 
 
@@ -266,13 +275,14 @@ static void Preferences_Event( void* ptr, int notification ) {
 		trap_Cvar_SetValue( "vr_showItemInHand", s_preferences.showinhand.curvalue);
         break;
 
-	case ID_SELECTORWITHHUD:
-		trap_Cvar_SetValue( "vr_weaponSelectorWithHud", s_preferences.selectorwithhud.curvalue);
-        break;
-
 	case ID_SHOWCONSOLE:
 		trap_Cvar_SetValue( "vr_showConsoleMessages", s_preferences.showconsole.curvalue);
         break;
+
+	case ID_PHYSICS:
+		trap_Cvar_SetValue( "ui_physics", s_preferences.physics.curvalue );
+		trap_Cvar_SetValue( "g_physics", s_preferences.physics.curvalue );
+		break;
 
 	case ID_BACK:
 		UI_PopMenu();
@@ -477,6 +487,16 @@ static void Preferences_MenuInit( void ) {
 	s_preferences.crosshaircolor.numitems			= 7;
 
 	y += BIGCHAR_HEIGHT+2;
+	s_preferences.physics.generic.type			= MTYPE_SPINCONTROL;
+	s_preferences.physics.generic.name			= "Physics:";
+	s_preferences.physics.generic.flags			= QMF_PULSEIFFOCUS|QMF_SMALLFONT;
+	s_preferences.physics.generic.callback		= Preferences_Event;
+	s_preferences.physics.generic.id			= ID_PHYSICS;
+	s_preferences.physics.generic.x				= PREFERENCES_X_POS;
+	s_preferences.physics.generic.y				= y;
+	s_preferences.physics.itemnames				= physics_names;
+
+	y += BIGCHAR_HEIGHT+2;
 	s_preferences.lasersight.generic.type        = MTYPE_RADIOBUTTON;
 	s_preferences.lasersight.generic.name	      = "Laser Sight:";
 	s_preferences.lasersight.generic.flags	      = QMF_PULSEIFFOCUS|QMF_SMALLFONT;
@@ -569,15 +589,6 @@ static void Preferences_MenuInit( void ) {
 	s_preferences.drawhud.itemnames			  = hud_names;
 
 	y += BIGCHAR_HEIGHT+2;
-    s_preferences.selectorwithhud.generic.type        = MTYPE_RADIOBUTTON;
-    s_preferences.selectorwithhud.generic.name	      = "Draw HUD On Weapon Wheel:";
-    s_preferences.selectorwithhud.generic.flags	      = QMF_PULSEIFFOCUS|QMF_SMALLFONT;
-    s_preferences.selectorwithhud.generic.callback    = Preferences_Event;
-    s_preferences.selectorwithhud.generic.id          = ID_SELECTORWITHHUD;
-    s_preferences.selectorwithhud.generic.x	          = PREFERENCES_X_POS;
-    s_preferences.selectorwithhud.generic.y	          = y;
-
-	y += BIGCHAR_HEIGHT+2;
     s_preferences.showconsole.generic.type        = MTYPE_RADIOBUTTON;
     s_preferences.showconsole.generic.name	      = "Show Console Messages:";
     s_preferences.showconsole.generic.flags	      = QMF_PULSEIFFOCUS|QMF_SMALLFONT;
@@ -586,7 +597,7 @@ static void Preferences_MenuInit( void ) {
     s_preferences.showconsole.generic.x	          = PREFERENCES_X_POS;
     s_preferences.showconsole.generic.y	          = y;
 
-	y += BIGCHAR_HEIGHT+16;
+	y += BIGCHAR_HEIGHT+2;
 	s_preferences.gore.generic.type		= MTYPE_SPINCONTROL;
 	s_preferences.gore.generic.flags		= QMF_PULSEIFFOCUS|QMF_SMALLFONT;
 	s_preferences.gore.generic.x			= PREFERENCES_X_POS - 120;
@@ -625,6 +636,7 @@ static void Preferences_MenuInit( void ) {
 	Menu_AddItem( &s_preferences.menu, &s_preferences.crosshair );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.crosshairsize );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.crosshaircolor );
+	Menu_AddItem( &s_preferences.menu, &s_preferences.physics );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.lasersight );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.simpleitems );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.wallmarks );
@@ -637,7 +649,6 @@ static void Preferences_MenuInit( void ) {
 	Menu_AddItem( &s_preferences.menu, &s_preferences.gore );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.damageeffect );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.showinhand );
-	Menu_AddItem( &s_preferences.menu, &s_preferences.selectorwithhud );
 	Menu_AddItem( &s_preferences.menu, &s_preferences.showconsole );
 
 	Menu_AddItem( &s_preferences.menu, &s_preferences.back );
