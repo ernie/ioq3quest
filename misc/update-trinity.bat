8fb3dce10000b3254ea83398b577985c74646301
Ernie Miller <ernie@ernie.io>
2026-05-24 14:00:54 -0400

    Add announcer pk3 to update-trinity scripts

--- misc/update-trinity.bat
+++ misc/update-trinity.bat
@@ -11,6 +11,7 @@ set "ASSETS=%SCRIPT_DIR%..\android\app\src\main\assets"
 echo Downloading latest Trinity pk3s...
 curl -fL -o "%ASSETS%\pak8t.pk3" "%BASE_URL%/pak8t.pk3" && echo   pak8t.pk3 OK || echo   pak8t.pk3 FAILED
 curl -fL -o "%ASSETS%\pak3t.pk3" "%BASE_URL%/pak3t.pk3" && echo   pak3t.pk3 OK || echo   pak3t.pk3 FAILED
+curl -fL -o "%ASSETS%\zzz-trinity-announcer.pk3" "%BASE_URL%/zzz-trinity-announcer.pk3" && echo   zzz-trinity-announcer.pk3 OK || echo   zzz-trinity-announcer.pk3 FAILED
 
 echo Done.
 pause
